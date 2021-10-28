dd7f43555c5bb3fe39d1b276796ada38495c3c3d
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2021-10-28 18:03:08 +0530

    working on tkinter library

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import PIL
 import datetime
 from PIL import Image, ImageTk
 from datetime import date
+import requests
+import time
+from tkinter.scrolledtext import ScrolledText
+ 
 
 
 
@@ -178,13 +182,44 @@ class Daata_Decorations():
 
     def Cancel_booking(self):
         cancel_book=tk.Tk()
+        today= date.today()
         cancel_book.geometry('800x800')
         cancel_book.title('CANCELLING')
+        back_frame=Frame(cancel_book, width=780, height=780, bg='black').place(x=10,y=10)
+        main_frame=Frame(cancel_book, width=760, height=760, bg='grey').place(x=20,y=20)
+        heading_frame=Frame(cancel_book, width=350, height=80, bg='White', relief=RAISED, bd=5).place(x=25,y=25)
+        search_frame=Frame(cancel_book, width=395, height=80, bg='White', relief=RAISED, bd=5).place(x=380,y=25)
+        bill_label=Label(cancel_book, text='Bill No:', bg='white', font=(20)).place(x=400,y=35)
+        bill_entry=Entry(cancel_book, width=16,font=(25)).place(x=500, y=35)
+
+        date_label=Label(cancel_book, text='Date:', bg='white', font=(20)).place(x=400,y=70)
+        cal=DateEntry(cancel_book,selectmode='day',year=today.year,date_pattern='dd/MM/yyyy',weekendbackground='white', month=today.month,showweeknumbers=False, weekendforeground='black', day=today.day,width=14, font=(15))
+        cal.place(x=500, y=70)
+
+        #down_frame=Frame(cancel_book, width=460, height=150, bg='White', relief=RAISED, bd=5).place(x=20,y=20)
+        label_heading=Label(cancel_book, text='Cancellation', bg='white',font=('arial', 30, 'bold')).place(x=70, y=40)
+        down_frame=Frame(cancel_book, width=750, height=600, bg='White', relief=RAISED, bd=5).place(x=25,y=110)
+        down_frame=Frame(cancel_book, width=750, height=60, bg='White', relief=RAISED, bd=5).place(x=25,y=710)
+
+        reset_btn=Button(cancel_book, width=15, text='Cancel', font=(8)).place(x=35, y=720)
+        reset_btn=Button(cancel_book, width=15, text='Exit',bg='red', font=(8),command=cancel_book.destroy).place(x=590, y=720)
         cancel_book.mainloop()
+
+
     def working_staff(self):
         working_staff=tk.Tk()
         working_staff.geometry('800x800')
         working_staff.title('STAFF')
+        back_frame=Frame(working_staff, width=780, height=780, bg='Black').place(x=10, y=10)
+        front_frame=Frame(working_staff, width=750, height=750).place(x=25, y=25)
+        heading_frame=Frame(working_staff, width=730, height=100, bg='white', relief=RIDGE, bd=5).place(x=35, y=40)
+        heading_label=Label(working_staff, text='WORKING STAFF', bg='white',font=('arail', 40, 'bold')).place(x=170, y=60)
+        main_frame=Frame(working_staff, width=730, height=520, bg='white', relief=RIDGE, bd=5).place(x=35, y=145)
+        btn_frame=Frame(working_staff, width=730, height=100, bg='white', relief=RIDGE, bd=5).place(x=35, y=670)
+        add_btn=Button(working_staff, width=15, text='Add', font=(8)).place(x=50, y=700)
+        delete_btn=Button(working_staff, width=15, text='Delete', font=(8)).place(x=330, y=700)
+        #modify_btn=Button(working_staff, width=15, text='Modify', font=(8)).place(x=450, y=700)
+        exit_btn=Button(working_staff, width=15, text='Exit',bg='red', font=(8),command=working_staff.destroy).place(x=570, y=700)
         working_staff.mainloop()
     def credit(self):
         credit=tk.Tk()
@@ -193,22 +228,107 @@ class Daata_Decorations():
         credit.mainloop()
     def setting(self):
         setting_win=tk.Tk()
-        setting_win.geometry('800x800')
+        setting_win.geometry('800x680')
         setting_win.title('SETTING')
+        setting_win['bg']='black'
+        lawn=StringVar(setting_win)
+        decoration=StringVar(setting_win)
+        gate=StringVar(setting_win)
+        watchme=StringVar(setting_win)
+        no=StringVar(setting_win)
+        cleaning=StringVar(setting_win)
+        electric=StringVar(setting_win)
+        water=StringVar(setting_win)
+        whatsapp=StringVar(setting_win)
+        emialid=StringVar(setting_win)
+        password=StringVar(setting_win)
+
+        tc = ttk.Notebook(setting_win)
+        t1 = ttk.Frame(tc)
+        tc.add(t1, text ='Booking Amount Setting')
+        tc.pack(expand = 1, fill ="both")
+        frame_main=Frame(t1, width=780, height=650, bg='black').place(x=10, y=10)
+        btn_frame=Frame(t1, width=760, height=520, bg='white', relief=SUNKEN, bd=5).place(x=20, y=30)
+        lbl=Label(t1, text="Lawn rent:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=50)
+        lawn__charges_spin = Spinbox(t1,textvariable=lawn, from_ = 0, to =9999, increment=50, width=5,font=(15))
+        lawn__charges_spin.place(x=250, y=55)
+        lbl=Label(t1, text="Lawn Decoration:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=100)
+        lawn__charges_spin = Spinbox(t1,textvariable=decoration, from_ = 0, to =9999, increment=50, width=5,font=(15))
+        lawn__charges_spin.place(x=250, y=105)
+        lbl=Label(t1, text="Gate + Stage:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=150)
+        lawn__charges_spin = Spinbox(t1,textvariable=gate, from_ = 0, to =9999, increment=50, width=5,font=(15))
+        lawn__charges_spin.place(x=250, y=155)
+        lbl=Label(t1, text="Watchman:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=200)
+        lawn__charges_spin = Spinbox(t1,textvariable=watchme, from_ = 0, to =9999, increment=50, width=5,font=(15))
+        lawn__charges_spin.place(x=250, y=205)
+        lbl=Label(t1, text="No.:", font=('Arail', 15, 'bold'), bg='white').place(x=350, y=200)
+        lawn__charges_spin = Spinbox(t1,textvariable=no, from_ = 0, to =9999, increment=1, width=5,font=(15))
+        lawn__charges_spin.place(x=450, y=205)
+        lbl=Label(t1, text="Cleaning Charges:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=250)
+        lawn__charges_spin = Spinbox(t1,textvariable=cleaning, from_ = 0, to =9999, increment=50, width=5,font=(15))
+        lawn__charges_spin.place(x=250, y=255)
+        lbl=Label(t1, text="Electric Charges:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=300)
+        lawn__charges_spin = Spinbox(t1,textvariable=electric, from_ = 0, to =9999, increment=50, width=5,font=(15))
+        lawn__charges_spin.place(x=250, y=305)
+        lbl=Label(t1, text="Water Charges:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=350)
+        lawn__charges_spin = Spinbox(t1,textvariable=water, from_ = 0, to =9999, increment=50, width=5,font=(15))
+        lawn__charges_spin.place(x=250, y=355)
+        lbl=Label(t1, text="Whatsapp Number:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=400)
+        whatsno_entry=Entry(t1,textvariable=whatsapp,font=('Arail', 15, 'bold'), width=25)
+        whatsno_entry.place(x=250, y=400)
+        lbl=Label(t1, text="Email ID:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=450)
+        Email_entry=Entry(t1,textvariable=emialid,font=('Arail', 15, 'bold'), width=25)
+        Email_entry.place(x=250, y=450)
+        lbl=Label(t1, text="Password:", font=('Arail', 15, 'bold'), bg='white').place(x=30, y=500)
+        passs_entry=Entry(t1,textvariable=password, font=('Arail', 15, 'bold'),width=25)
+        passs_entry.place(x=250, y=500)
+        
+
+
+        btn_frame=Frame(t1, width=760, height=70, bg='white', relief=SUNKEN, bd=5).place(x=20, y=560)
+        Exit_button=Button(t1, text='Exit',bg='Red',fg='White',height=2, width=15,font=('arial', 10, 'bold'),command=setting_win.destroy)
+        Exit_button.place(x=620, y=575)
+        Update_button=Button(t1, text='Update',bg='Green',fg='White',height=2, width=15,font=('arial', 10, 'bold'))
+        Update_button.place(x=470, y=575)
+
         setting_win.mainloop()
    
     def __init__(self):
+        
+        
         
         today= date.today()
         events={'2021-10-28':('London','Program'),\
                 '2021-10-15':('Paris','Program'),\
                 '2018-07-30':('New York','Program')}
+
+        
         self.main_window=Tk()
         self.dd=StringVar()
         self.mm=StringVar()
         self.yy=StringVar()
         self.show_result=StringVar()
-        
+        url = "https://www.geeksforgeeks.org"
+        timeout = 1
+        try:
+            request=requests.get(url, timeout=timeout)
+            image1 = Image.open("wifi.png")
+            resize_image=image1.resize((50,40), Image.ANTIALIAS)
+            test = ImageTk.PhotoImage(resize_image)
+
+            label1 = tk.Label(image=test)
+            label1.image = test
+            label1.place(x=1450,y=780)
+
+        # catching exception
+        except (requests.ConnectionError, requests.Timeout) as exception:
+            image1 = Image.open("no_wifi.png")
+            resize_image=image1.resize((50,40), Image.ANTIALIAS)
+            test = ImageTk.PhotoImage(resize_image)
+            label1 = tk.Label(image=test)
+            label1.image = test
+            label1.place(x=1450,y=780)
+            
 
         #self.main_window.overrideredirect(True)
        
@@ -272,11 +392,60 @@ class Daata_Decorations():
         self.button=Button(self.main_window, text='Exit',bg='red',fg='white',height=2, width=15,command=self.main_window.destroy,font=('arial', 10, 'bold'))
         self.button.place(x=1380, y=142)
 
-        treeview_frame=Frame(self.main_window, width=1020, height=570, bg='white',bd=8, relief= RIDGE).place(x=500, y=200)
+        self.treeview_frame=Frame(self.main_window, width=1020, height=570, bg='white',bd=8, relief= RIDGE).place(x=500, y=200)
+        self.column_notice=('#1','#2','#3','#4','#5', '#6', '#7', '#8')
+        self.tree_note=ttk.Treeview(self.main_window, columns=self.column_notice, show='headings', height=23)
+        self.tree_note.column('#1',anchor=CENTER, width=70)
+        self.tree_note.column('#2',anchor=CENTER, width=285)
+        self.tree_note.column('#3',anchor=CENTER, width=120)
+        self.tree_note.column('#4',anchor=CENTER, width=100)
+        self.tree_note.column('#5',anchor=CENTER, width=150)
+        self.tree_note.column('#6',anchor=CENTER, width=70)
+        self.tree_note.column('#7',anchor=CENTER, width=100)
+        self.tree_note.column('#8',anchor=CENTER, width=100)
+        
+        self.tree_note.heading('#1', text='Bill No')
+        self.tree_note.heading('#2', text='Party Name')
+        self.tree_note.heading('#3', text='Booking Date')
+        self.tree_note.heading('#4', text='Phone No')
+        self.tree_note.heading('#5', text='Occasion')
+        self.tree_note.heading('#6', text='Person')
+        self.tree_note.heading('#7', text='Advance')
+        self.tree_note.heading('#8', text='Pending')
+        
+
+        self.tree_note.bind('<<TreeviewSelect>>')
+        self.tree_note.place(x=510, y=212)
         #frame2=Frame(self.maipn_window, width=500, height=100, bg='blue').place(x=1,y=30)
         lbs1=Label(self.main_window, text='DAATA BICHAYAT & DECORATION WORKS', font=('courier', 25, 'bold'), bg='blue', fg='White').place(x=450, y=780)
         self.main_window.resizable(0,0)
+        '''if request==1:
+            image1 = Image.open("wifi.png")
+            resize_image=image1.resize((50,40), Image.ANTIALIAS)
+            test = ImageTk.PhotoImage(resize_image)
+
+            label1 = tk.Label(image=test)
+            label1.image = test
+            
+
+            # Position image
+            label1.place(x=40,y=40)
+        else:
+            image1 = Image.open("no_wifi.png")
+            resize_image=image1.resize((50,40), Image.ANTIALIAS)
+            test = ImageTk.PhotoImage(resize_image)
+
+            label1 = tk.Label(image=test)
+            label1.image = test
+
+            # Position image
+            label1.place(x=40,y=40)'''
+      
+       
+        
+
         self.main_window.mainloop()
 
 if __name__ == "__main__":
     dawat_Lawn=Daata_Decorations()
+    
